9c159c78b22831e91ea01e83e223b877e301eb55
abhishek <abhishek@abhishek-Inspiron-3542>
2018-07-04 17:27:37 +0530

    Added Trash_3

---
+++ Trash_3/vector.cpp
@@ -0,0 +1,17 @@
+// vector.cpp
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector <int> my_vect;
+
+	cout << my_vect.size() << endl;
+	cout << my_vect.capacity() << endl;
+	cout << my_vect.resize(5, 10) << endl;
+
+	return 0;
+}
